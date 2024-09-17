71e6dc0a9804cc7636b2a3b81bb18d31fd59ca4a
jmat436 <jmattern.436@gmail.com>
2024-09-17 14:56:38 +0200

    Initial commit for my Xcode iOS app

---
+++ ConciousConsumer/ConciousConsumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConciousConsumerApp.swift
+//  ConciousConsumer
+//
+//  Created by Jamie Mattern on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ConciousConsumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
